492651de85c0a56956543e3d58885eae9cbe4031
Biswadeep Roy <74821633+biswadeep-roy@users.noreply.github.com>
2024-10-07 17:50:14 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,178 @@
+
+
+const logotext = "BR"; 
+
+const meta = {
+    title: "Biswadeep Roy",
+    description: "Passionate Data Scientist and Full Stack Developer from India. Expertise in machine learning, AI, and crafting intelligent web solutions using technologies like Python, JavaScript, React, and more.",
+};
+
+const introdata = {
+    title: "I’m Biswadeep Roy",
+    animated: {
+        first: "Passionate about data science, AI,",
+        second: "and web development, I specialize in crafting",
+        third: "intelligent solutions using machine learning technologies.",
+        fourth: "I excel in creating responsive web applications",
+        fifth: "using technologies like HTML, CSS, Bootstrap,",
+        sixth: "JavaScript, React, Node.js, and Express.js.",
+        seventh: "My skills also include version control",
+        eighth: "with Git, containerization with Docker,",
+        ninth: "and orchestration with Kubernetes.",
+        tenth: "In the cloud space, I leverage platforms",
+        eleventh: "such as AWS and Oracle Cloud for seamless deployment."
+    },
+    description: "As a data science enthusiast with a focus on AI and ML, I bring a unique blend of skills to the table. Proficient in Python for developing intelligent solutions, my expertise extends to crafting impactful web applications. Whether it's leveraging machine learning algorithms or creating seamless user experiences, I am dedicated to pushing the boundaries of technology.",
+    your_img_url: "https://avatars.githubusercontent.com/u/74821633?v=4",
+    
+
+
+
+
+};
+
+const dataabout = {
+    title: "A Bit About Myself",
+    aboutme: "Hi, I'm Biswadeep Roy—a highly skilled software engineer with expertise in machine learning, data analysis, and full-stack web development. With proficiency in technologies like Python, TensorFlow, React, and Node.js, I have experience building scalable applications and leveraging data-driven insights to create impactful solutions. Let's innovate and turn ideas into reality through the power of technology!"
+};
+
+
+const worktimeline = [{
+        jobtitle: "Freelance Web Developer",
+        where: "Self-Employed",
+        date: "2023 - Present",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+ 
+];
+
+
+const skills = [
+    { name: "Python", value: 90 },
+    { name: "TensorFlow", value: 85 },
+    { name: "React.js", value: 80 },
+    { name: "Node.js", value: 80 },
+    { name: "MongoDB", value: 75 },
+    { name: "AWS Cloud", value: 70 },
+    { name: "MySQL", value: 75 },
+    { name: "NLP", value: 70 },
+    { name: "OOPS", value: 80 },
+    { name: "Express.js", value: 75 },
+    { name: "Deep Learning", value: 80 },
+    { name: "Computer Vision", value: 75 },
+    { name: "DialogFlow", value: 70 },
+    { name: "Javascript", value: 85 },
+    { name: "Github", value: 85 },
+    { name: "Yolov8", value: 80 }
+];
+
+
+
+
+const services = [
+    {
+        title: "Machine Learning & AI Solutions",
+        description: "Leveraging machine learning and AI technologies like TensorFlow and NLP to build smart applications. Specializing in deep learning, computer vision, and AI-powered solutions for various domains.",
+    },
+    {
+        title: "Full Stack Web Development",
+        description: "Building robust web applications using modern technologies such as React.js, Node.js, and MongoDB. Expertise in both front-end and back-end development, ensuring seamless user experiences and scalable infrastructure.",
+    },
+    {
+        title: "Cloud & Data Solutions",
+        description: "Providing cloud-based data solutions using AWS Cloud services. Skilled in data analysis, database management, and developing scalable applications leveraging cloud infrastructure and services.",
+    },
+    {
+        title: "Computer Vision Applications",
+        description: "Developing advanced computer vision solutions using Yolov8 and other cutting-edge tools to enable real-time object detection, classification, and analysis for various industries.",
+    }
+];
+
+const dataportfolio = [
+    {
+        img: require("../src/images/yolo.jpg"),
+        description: "Cars-Counter-using-YOLOv8: This repository contains Python code to count the number of cars, trucks, buses, and motorbikes in a video using YOLOv8 object detection. It also tracks the detected vehicles and displays the count in real-time.",
+        link: "https://github.com/biswadeep-roy/Cars-Counter-using-YOLOv8/"
+    },
+    {
+        img: require("../src/images/carpool.jpeg"),
+        description: "Carpool: Carpool is a web application that facilitates carpooling among students and staff within a university setting. Users can register their carpool details, including their availability, campus, and preferred locations.",
+        link: "https://github.com/biswadeep-roy/carpool"
+    },
+    {
+        img: require("../src/images/bakery.png"),
+        description: "Bakery MERN: Heavenly Bites is a full-featured bakery app using the MERN stack, offering seamless online cake browsing, ordering, and management.",
+        link: "https://github.com/biswadeep-roy/bakery-mern"
+    },
+    {
+        img: require("../src/images/plant.png"),
+        description: "Plant-Health-Classifier: This project is a deep learning model that identifies whether a plant is healthy or not by analyzing photos of its leaves. It's trained to classify plants into various categories based on their health status.",
+        link: "https://github.com/biswadeep-roy/Plant-Health-Classifier"
+    },
+    {
+        img: require("../src/images/news.png"),
+        description: "BNews: A customizable news web and mobile app to keep you up-to-date with global events and personalize your news feed based on categories of your choice.",
+        link: "https://github.com/biswadeep-roy/News-App"
+    },
+    {
+        img: require("../src/images/safety.png"),
+        description: "Safety-Detection-YOLOv8: A cutting-edge object detection model to enhance safety measures by identifying and classifying personal protective equipment.",
+        link: "https://github.com/biswadeep-roy/Safety-Detection-YOLOv8"
+    },
+    {
+        img: require("../src/images/security.png"),
+        description: "Security Cam: This Python app uses PyQt5 and OpenCV to create a security camera system that monitors webcam feed, detects motion, and saves images upon detecting significant motion.",
+        link: "https://github.com/biswadeep-roy/security-cam"
+    },
+    {
+        img: require("../src/images/handwriting.png"),
+        description: "Text-to-Handwriting-Converter: A Python script to convert plain text into handwritten-style images.",
+        link: "https://github.com/biswadeep-roy/Text-to-Handwriting-Converter"
+    },
+    {
+        img: require("../src/images/doctor.jpg"),
+        description: "Doctor-Appointment-MERN: A web application to simplify booking medical appointments by connecting patients with healthcare professionals.",
+        link: "https://github.com/biswadeep-roy/Doctor-Appointment-Mern"
+    }
+];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "biswadeeproy1230@gmail.com",
+    YOUR_FONE: "+918473868771",
+    description: "Feel free to reach out to me for any inquiries, collaborations, or just to say hello! I'm passionate about technology and always open to new opportunities. Looking forward to connecting with you!",
+    YOUR_SERVICE_ID: "service_bsvtmpr",
+    YOUR_TEMPLATE_ID: "template_7jytrxr",
+    YOUR_USER_ID: "Aoa5IhymUk2Hqhj-n",
+};
+
+const socialprofils = {
+    github: "https://github.com/biswadeep-roy/",
+    facebook: "https://www.facebook.com/profile.php?id=61553915821870&mibextid=ZbWKwL/",
+    linkedin: "https://www.linkedin.com/in/biswadeep-roy-022804219/",
+    twitter: "https://twitter.com/biswadeep___roy/",
+    instagram: "https://www.instagram.com/_biebs__/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
+
+
